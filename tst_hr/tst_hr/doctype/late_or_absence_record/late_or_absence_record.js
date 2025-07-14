0cba1be070e89a21917aef0cf6c73d50102a8ac6
a.mansy <ahmedmansy265@gmail.com>
2025-07-14 20:31:02 +0300

    changes

--- tst_hr/tst_hr/doctype/late_or_absence_record/late_or_absence_record.js
+++ tst_hr/tst_hr/doctype/late_or_absence_record/late_or_absence_record.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2025, TST Developer and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Late or Absence Record", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Late or Absence Record", {
+	absence(frm) {
+        if (frm.doc.absence){
+            frm.set_value("late",0)
+            
+        }
+    },
+    late(frm) {
+        if (frm.doc.late){
+            frm.set_value("absence",0)
+        }
+    },
+});

e97458f085b4915e608221ae268977142b0fcaf6
Ahmed-Mansy-Mansico <ahmedmansy265@gmail.com>
2025-07-25 00:50:10 +0300

    filter fields

--- tst_hr/tst_hr/page/organization_chart/organization_chart.js
+++ tst_hr/tst_hr/page/organization_chart/organization_chart.js
@@ -1,97 +1,256 @@
-frappe.pages["organization-chart"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: __("Organizational Chart"),
-		single_column: true,
-	});
+frappe.pages["organization-chart"].on_page_load = function(wrapper) {
+    const page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: __("Organizational Chart"),
+        single_column: true,
+    });
 
-	const department_filter = page.add_field({
-		label: "Department",
-		fieldname: "department",
-		fieldtype: "Link",
-		options: "Department",
-		change() {
-			render_chart();
-		}
-	});
+    let organizational_chart;
 
-	const branch_filter = page.add_field({
-		label: "Branch",
-		fieldname: "branch",
-		fieldtype: "Link",
-		options: "Branch",
-		change() {
-			render_chart();
-		}
-	});
+    $(wrapper).bind("show", () => {
+        frappe.require("hierarchy-chart.bundle.js", () => {
+            const method = "tst_hr.tst_hr.page.organization_chart.organization_chart.get_children";
+            
+            class SafeHierarchyChart extends hrms.HierarchyChart {
+                render_node(node, parent_node) {
+                    try {
+                        if (!parent_node) {
+                            parent_node = this.create_default_container();
+                        }
+                        return super.render_node(node, parent_node);
+                    } catch (error) {
+                        console.error("Error rendering node:", error);
+                        return null;
+                    }
+                }
+                
+                create_default_container() {
+                    const container = document.createElement('div');
+                    container.className = 'org-chart-default-container';
+                    this.container.appendChild(container);
+                    return container;
+                }
+                
+                show() {
+                    this.setup_actions();
+                    if (this.page.main.find('[data-fieldname="company"]').length) return;
+                    
+                    let me = this;
+                    let filters = {};
 
-	const section_filter = page.add_field({
-		label: "Section",
-		fieldname: "section",
-		fieldtype: "Link",
-		options: "Sections",
-		change() {
-			render_chart();
-		}
-	});
-
-	let organizational_chart;
-
-	$(wrapper).bind("show", () => {
-		frappe.require("hierarchy-chart.bundle.js", () => {
-			const method = "tst_hr.tst_hr.page.organization_chart.organization_chart.get_children";
-
-			if (frappe.is_mobile()) {
-				organizational_chart = new hrms.HierarchyChartMobile("Employee", wrapper, method);
-			} else {
-				organizational_chart = new hrms.HierarchyChart("Employee", wrapper, method);
-			}
-
-			frappe.breadcrumbs.add("HR");
-            organizational_chart.show();
-
-			// Override show to add filters
-			organizational_chart.show = function (filters = {}) {
-				const args = {
-					parent: "",
-					company: frappe.defaults.get_user_default("company"),
-					...filters
-				};
-
-				// Call frappe.call with your filters
-				frappe.call({
-					method: this.method,
-					args: args,
-					callback: (r) => {
-						if (r.message) {
-                            if (typeof this._draw === "function") {
-                                this._draw(r.message);
-                            } else if (typeof this.render === "function") {
-                                this.render(r.message);
-                            } else {
-                                this.data = r.message; // Update internal data if possible
-                                console.log("Organizational chart updated data:", r.message);
-                                // Optionally try refreshing manually
+                    // Company Filter
+                    let company = this.page.add_field({
+                        fieldtype: "Link",
+                        options: "Company",
+                        fieldname: "company",
+                        placeholder: __("Select Company"),
+                        default: frappe.defaults.get_default("company"),
+                        reqd: 1,
+                        change: () => {
+                            me.company = company.get_value();
+                            $("#hierarchy-chart-wrapper").remove();
+                            if (me.company) {
+                                me.make_svg_markers();
+                                me.setup_hierarchy();
+                                me.render_root_nodes();
+                                me.all_nodes_expanded = false;
                             }
                         }
+                    });
 
-					}
-				});
+                    // Department Filter
+                    let department = this.page.add_field({
+                        label: "Department",
+                        fieldname: "department",
+                        fieldtype: "Link",
+                        options: "Department",
+                        only_select: true,
+                        change: () => {
+                            me.department = department.get_value();
+                            $("#hierarchy-chart-wrapper").remove();
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                            me.all_nodes_expanded = false;
+                        }
+                    });
+
+                    // Designation Filter
+                    let designation = this.page.add_field({
+                        label: "Designation",
+                        fieldname: "designation",
+                        fieldtype: "Link",
+                        options: "Designation",
+                        only_select: true,
+                        change: () => {
+                            me.designation = designation.get_value();
+                            $("#hierarchy-chart-wrapper").remove();
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                            me.all_nodes_expanded = false;
+                        }
+                    });
+
+                    // Branch Filter
+                    let branch = this.page.add_field({
+                        label: "Branch",
+                        fieldname: "branch",
+                        fieldtype: "Link",
+                        options: "Branch",
+                        only_select: true,
+                        change: () => {
+                            me.branch = branch.get_value();
+                            $("#hierarchy-chart-wrapper").remove();
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                            me.all_nodes_expanded = false;
+                        }
+                    });
+
+                    // Style all filters consistently
+                    $('[data-fieldname="company"], [data-fieldname="department"], [data-fieldname="designation"], [data-fieldname="branch"]')
+                        .find('.control-input')
+                        .css({
+                            "position": "relative",
+                            "z-index": 2
+                        });
+
+                    company.refresh();
+                    department.refresh();
+                    designation.refresh();
+                    branch.refresh();
+
+                    $(`[data-fieldname="company"]`).trigger("change");
+					$(`[data-fieldname="company"]`).trigger("change");
+					$(`[data-fieldname="company"] .link-field`).css("z-index", 1);
+					$(`[data-fieldname="department"] .link-field`).css("z-index", 1);
+					$(`[data-fieldname="branch"] .link-field`).css("z-index", 1);
+					$(`[data-fieldname="designation"] .link-field`).css("z-index", 1);
+                }
+            }
+
+            class SafeHierarchyChartMobile extends hrms.HierarchyChartMobile {
+                render_node(node, parent_node) {
+                    try {
+                        if (!parent_node) {
+                            parent_node = this.create_default_container();
+                        }
+                        return super.render_node(node, parent_node);
+                    } catch (error) {
+                        console.error("Mobile node error:", error);
+                        return null;
+                    }
+                }
                 
-			};
+                create_default_container() {
+                    const container = $('<div class="mobile-default-container"></div>');
+                    this.$sibling_group.append(container);
+                    return container[0];
+                }
+                
+                show() {
+                    if (this.page.main.find('[data-fieldname="company"]').length) return;
+                    
+                    let me = this;
+                    let filters = {};
 
-			// Expose render_chart globally
-			window.render_chart = function () {
-				const filters = {
-					department: department_filter.get_value(),
-					branch: branch_filter.get_value(),
-					section: section_filter.get_value()
-				};
-				organizational_chart.show(filters);
-			};
+                    // Company Filter
+                    let company = this.page.add_field({
+                        fieldtype: "Link",
+                        options: "Company",
+                        fieldname: "company",
+                        placeholder: __("Select Company"),
+                        default: frappe.defaults.get_default("company"),
+                        only_select: true,
+                        reqd: 1,
+                        change: () => {
+                            me.company = company.get_value();
+                            if (me.$sibling_group) me.$sibling_group.remove();
+                            me.$sibling_group = $(`<div class="sibling-group mt-4 mb-4"></div>`);
+                            me.page.main.append(me.$sibling_group);
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                        }
+                    });
 
-			// Initial load
-			render_chart();
-		});
-	});
-};
+                    // Department Filter
+                    let department = this.page.add_field({
+                        label: "Department",
+                        fieldname: "department",
+                        fieldtype: "Link",
+                        options: "Department",
+                        only_select: true,
+                        change: () => {
+                            me.department = department.get_value();
+                            if (me.$sibling_group) me.$sibling_group.remove();
+                            me.$sibling_group = $(`<div class="sibling-group mt-4 mb-4"></div>`);
+                            me.page.main.append(me.$sibling_group);
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                        }
+                    });
+
+                    // Designation Filter
+                    let designation = this.page.add_field({
+                        label: "Designation",
+                        fieldname: "designation",
+                        fieldtype: "Link",
+                        options: "Designation",
+                        only_select: true,
+                        change: () => {
+                            me.designation = designation.get_value();
+                            if (me.$sibling_group) me.$sibling_group.remove();
+                            me.$sibling_group = $(`<div class="sibling-group mt-4 mb-4"></div>`);
+                            me.page.main.append(me.$sibling_group);
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                        }
+                    });
+
+                    // Branch Filter
+                    let branch = this.page.add_field({
+                        label: "Branch",
+                        fieldname: "branch",
+                        fieldtype: "Link",
+                        options: "Branch",
+                        only_select: true,
+                        change: () => {
+                            me.branch = branch.get_value();
+                            if (me.$sibling_group) me.$sibling_group.remove();
+                            me.$sibling_group = $(`<div class="sibling-group mt-4 mb-4"></div>`);
+                            me.page.main.append(me.$sibling_group);
+                            me.make_svg_markers();
+                            me.setup_hierarchy();
+                            me.render_root_nodes();
+                        }
+                    });
+
+                    company.refresh();
+                    department.refresh();
+                    designation.refresh();
+                    branch.refresh();
+
+                    $(`[data-fieldname="company"]`).trigger("change");
+					$(`[data-fieldname="company"] .link-field`).css("z-index", 1);
+					$(`[data-fieldname="department"] .link-field`).css("z-index", 1);
+					$(`[data-fieldname="branch"] .link-field`).css("z-index", 1);
+					$(`[data-fieldname="designation"] .link-field`).css("z-index", 1);
+                }
+            }
+
+            if (frappe.is_mobile()) {
+                organizational_chart = new SafeHierarchyChartMobile("Employee", wrapper, method);
+            } else {
+                organizational_chart = new SafeHierarchyChart("Employee", wrapper, method);
+            }
+
+            frappe.breadcrumbs.add("HR");
+            organizational_chart.show();
+        });
+    });
+};	
--- tst_hr/tst_hr/page/organization_chart/organization_chart.py
+++ tst_hr/tst_hr/page/organization_chart/organization_chart.py
@@ -1,61 +1,93 @@
 import frappe
 from frappe.query_builder.functions import Count
 
-
 @frappe.whitelist()
-def get_children(parent=None, company=None, exclude_node=None, department=None, branch=None, section=None):
-    frappe.msgprint(str(department))
-    filters = [["status", "=", "Active"]]
+def get_children(parent=None, company=None, exclude_node=None, department=None, designation=None, branch=None):
+    # Always return at least an empty list
+    try:
+        filters = [["status", "=", "Active"]]
+        
+        # Apply filters
+        if company and company != "All Companies":
+            filters.append(["company", "=", company])
+        
+        if department and department != "All Departments":
+            filters.append(["department", "=", department])
+        
+        # Fix typo: "designation" instead of "designation"
+        if designation and designation != "All Designations":
+            filters.append(["designation", "=", designation])
 
-    if company and company != "All Companies":
-        filters.append(["company", "=", company])
+        if branch and branch != "All Branches":
+            filters.append(["branch", "=", branch])
 
-    if parent and company and parent != company:
-        filters.append(["reports_to", "=", parent])
-    else:
-        filters.append(["reports_to", "=", ""])
+        # Improved parent node handling
+        if parent and parent != "null" and parent != "undefined":
+            filters.append(["reports_to", "=", parent])
+        else:
+            filters.append(["reports_to", "=", ""])  # Get root nodes
 
-    if exclude_node:
-        filters.append(["name", "!=", exclude_node])
+        if exclude_node:
+            filters.append(["name", "!=", exclude_node])
 
-    # ✅ Apply filters from the frontend
-    if department:
-        filters.append(["department", "=", department])
-    if branch:
-        filters.append(["branch", "=", branch])
-    if section:
-        filters.append(["custom_section", "=", section])
+        employees = frappe.get_all(
+            "Employee",
+            fields=[
+                "employee_name as name",
+                "name as id",
+                "lft",
+                "rgt",
+                "reports_to",
+                "image",
+                "designation as title",
+                "department",
+                "designation",
+                "branch"
+            ],
+            filters=filters,
+            order_by="name",
+        )
 
-    employees = frappe.get_all(
-        "Employee",
-        fields=[
-            "employee_name as name",
-            "name as id",
-            "lft",
-            "rgt",
-            "reports_to",
-            "image",
-            "designation as title",
-        ],
-        filters=filters,
-        order_by="name",
-    )
+        for employee in employees:
+            employee.connections = get_connections(
+                employee.id, 
+                employee.lft, 
+                employee.rgt,
+                department=department,
+                designation=designation,
+                branch=branch
+            )
+            employee.expandable = bool(employee.connections)
 
-    for employee in employees:
-        employee.connections = get_connections(employee.id, employee.lft, employee.rgt)
-        employee.expandable = bool(employee.connections)
+        return employees or []  # Ensure we always return a list
 
-    return employees
+    except Exception as e:
+        frappe.log_error(f"Error in get_children: {str(e)}")
+        return []
 
+def get_connections(employee: str, lft: int, rgt: int, department=None, designation=None, branch=None) -> int:
+    try:
+        Employee = frappe.qb.DocType("Employee")
+        query = (
+            frappe.qb.from_(Employee)
+            .select(Count(Employee.name))
+            .where(
+                (Employee.lft > lft) & 
+                (Employee.rgt < rgt) & 
+                (Employee.status == "Active")
+            )
+        )
+        
+        if department and department != "All Departments":
+            query = query.where(Employee.department == department)
 
+        if designation and designation != "All Designations":
+            query = query.where(Employee.designation == designation)
 
+        if branch and branch != "All Branches":
+            query = query.where(Employee.branch == branch)
 
-def get_connections(employee: str, lft: int, rgt: int) -> int:
-    Employee = frappe.qb.DocType("Employee")
-    query = (
-        frappe.qb.from_(Employee)
-        .select(Count(Employee.name))
-        .where((Employee.lft > lft) & (Employee.rgt < rgt) & (Employee.status == "Active"))
-    ).run()
-
-    return query[0][0]
+        return query.run()[0][0] or 0
+    except Exception as e:
+        frappe.log_error(f"Error in get_connections: {str(e)}")
+        return 0
